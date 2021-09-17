6039e45a5f60d30beac3e156dc9e4ce12d07ada7
JerryPopi <djccowcow@gmail.com>
2021-09-17 22:11:35 +0300

    Sexy sex

--- commands.py
+++ commands.py
@@ -9,21 +9,28 @@ async def ping(*args, msg, client):
     await msg.channel.send('pong')
 
 async def play(*args, msg, client):
-    link = args[0]
-    with tempfile.TemporaryDirectory() as tmpdir:
-        file = path.join(tmpdir, 'audio.opus')
-        run(['youtube-dl', '-x', '--audio-format', 'opus', link, '-o', file])
+    def track_end(error):
+        if error != None:
+            print(error)
+        else:
+            print("Finished track")
+
 
     channel = msg.author.voice.channel
 
+    link = args[0]
+    from os import listdir
+    # with tempfile.TemporaryDirectory() as tmpdir:
+    tmpdir = tempfile.TemporaryDirectory()
+    file = path.join(tmpdir.name, 'audio.opus')
+    run(['youtube-dl', '-x', '--audio-format', 'opus', link, '-o', file])
+
+
     if channel != None:
         vc = await channel.connect()
-        # player = await vc.FFmpegPCMAudio('a_nightmare.opus')
-        # player.start()
-        # while not player.is_done():
-            # await time.sleep(1)
-        # player.stop()
 
-        vc.play(FFmpegPCMAudio('a_nightmare.mp3'))
+        vc.play(FFmpegPCMAudio(file), after=lambda e: tmpdir.cleanup())
 
-        # await client.voice_client.disconnect()            
+
+
+        
