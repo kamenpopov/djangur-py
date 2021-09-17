94c829f6f0b2215151c90f26da6bfae2503d0448
teoet6 <teoet6@protonmail.com>
2021-09-17 21:23:22 +0300

    audio files downloading from youtube

--- commands.py
+++ commands.py
@@ -1,2 +1,13 @@
+from subprocess import run
+import tempfile
+from os import path
+
 async def ping(*args, msg, client):
     await msg.channel.send('pong')
+
+async def play(*args, msg, client):
+    link = args[0]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        file = path.join(tmpdir, 'audio.opus')
+        run(['youtube-dl', '-x', '--audio-format', 'opus', link, '-o', file])
+        # PLAY FILE HERE
