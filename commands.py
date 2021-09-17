ab105be7c1b7f8b9d29c9b5ee616e849ddac9846
teoet6 <teoet6@protonmail.com>
2021-09-17 23:14:54 +0300

    channel

--- commands.py
+++ commands.py
@@ -4,13 +4,37 @@ from subprocess import run
 import tempfile
 from os import path
 
+class Guild_Instance():
+    def __init__(self, vc=None):
+        self.vc = vc
+
+
+    async def connect(self, channel):
+        if channel is None:
+            return
+
+        if self.vc is None:
+            self.vc = await channel.connect()
+        elif channel.id != self.vc.channel.id:
+            await self.vc.disconnect()
+            self.vc = await channel.connect()
+
+
+guild_instances = {}
+
+
 async def ping(*args, msg, client):
     await msg.channel.send('pong')
 
+
 async def play(*args, msg, client):
-    channel = msg.author.voice.channel
-    if channel == None:
-        return
+    global guild_instances
+
+    if msg.guild.id not in guild_instances:
+        guild_instances[msg.guild.id] = Guild_Instance()
+    ginstance = guild_instances[msg.guild.id]
+
+    await ginstance.connect(msg.author.voice.channel)
 
     link = ' '.join(args)
 
@@ -20,5 +44,4 @@ async def play(*args, msg, client):
 
     print(f'"{link}" downloaded!')
 
-    vc = await channel.connect()
-    vc.play(FFmpegPCMAudio(file), after=lambda e: tmpdir.cleanup())
+    ginstance.vc.play(FFmpegPCMAudio(file), after=lambda e: tmpdir.cleanup())
